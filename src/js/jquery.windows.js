de137c17d6f81f996c85f597dfb74e14603f3314
Puzzlet Chung <puzzlet@gmail.com>
2013-03-09 20:47:23 +0900

    maximize

--- src/js/jquery.windows.js
+++ src/js/jquery.windows.js
@@ -2,9 +2,64 @@
 
 $.widget("win.window", {
     _create: function() {
-        if (! this.element.hasClass("maximized")) {
-            this.element.draggable({ handle: ".title-bar" });
+        this.element.draggable({ handle: ".title" });
+        if (this.element.hasClass( "maximized" )) {
+            this.element.draggable( "disable" );
         }
+        this._on({
+            "click .title-button.maximize": function( event ) {
+                this.maximize();
+            },
+            "click .title-button.restore": function( event ) {
+                this.restore();
+            },
+            "click .title-button.close": function( event ) {
+                this.element.hide();
+            },
+            "dblclick .title": function( event ) {
+                if (this.element.hasClass( "maximized" )) {
+                    this.restore();
+                }
+                else {
+                    this.maximize();
+                }
+            },
+        });
+    },
+
+    maximize: function() {
+        // TODO: animation
+        this._offset = this.element.offset();
+        this._width = this.element.width();
+        this._height = this.element.height();
+        var taskbar_height = $( ".taskbar" ).height();
+        this.element
+            .draggable( "disable" )
+            .addClass( "maximized" )
+            .css({
+                left: "auto",
+                top: "0",
+                width: "100%",
+                height: "auto",
+                bottom: taskbar_height + "px",
+            });
+        this.element.find( ".title-button.maximize" )
+            .removeClass( "maximize" )
+            .addClass( "restore" );
+    },
+
+    restore: function() {
+        // TODO: animation
+        this.element
+            .removeClass( "maximized" )
+            .draggable( "enable" )
+            .css({ bottom: "auto" })
+            .offset( this._offset )
+            .width( this._width )
+            .height( this._height );
+        this.element.find( ".title-button.restore" )
+            .removeClass( "restore" )
+            .addClass( "maximize" );
     },
 });
 
