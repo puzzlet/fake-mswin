5dffabc41f18a90a063aef23dbb344ba108383a3
Puzzlet Chung <puzzlet@gmail.com>
2013-03-31 21:27:51 +0900

    fix taskbar button abnormaly

--- src/js/jquery.mswin.js
+++ src/js/jquery.mswin.js
@@ -28,7 +28,7 @@ $.widget( "mswin.window", {
             },
             "click .title-button.minimize": function( event ) {
                 this.element.hide();
-                this.unselect();
+                this.unselect( this.element );
             },
             "click .title-button.maximize": function( event ) {
                 this.maximize();
@@ -57,11 +57,12 @@ $.widget( "mswin.window", {
         else {
             this.restore();
         }
-        this.task_element = $("<div class='task'></div>");
+        this.task_element = $( "<div class='task'></div>" );
+        this.task_element.attr("for", this.element.attr( "id" ));
         var tasks = this.taskbar.find(".tasks");
         tasks.append(this.task_element);
 
-        var title_icon = this.element.find(".title-icon");
+        var title_icon = this.element.find( ".title-icon" );
         var title_icon_clone = title_icon.clone();
         var p = 'background-image';
         title_icon_clone.css(p, title_icon.css(p));
@@ -70,7 +71,7 @@ $.widget( "mswin.window", {
 
         this.task_element
             .append(title_icon_clone)
-            .append($("<div class='title-wrapper'><span>" + this.element.find(".title").text() + "</span></div>"))
+            .append($( "<div class='title-wrapper'><span>" + this.element.find(".title").text() + "</span></div>" ))
             .on("click", $.proxy( this.taskClick, this ));
         this.select();
     },
@@ -118,28 +119,33 @@ $.widget( "mswin.window", {
     },
 
     select: function() {
+        var _this = this;
         $( ":mswin-window .title-bar.ui-selected" ).removeClass( "ui-selected" );
         this.element.find( ".title-bar" ).addClass( "ui-selected" );
         if (!this.element.is( ":visible" )) {
             this.element.show();
         }
         // taskbar
-        $( ":mswin-window" ).each( function() {
-            $( this ).data( "mswin-window" ).unselect();
+        $( ".window" ).each( function() {
+            _this.unselect( this );
         });
         this.task_element.addClass( "selected" );
         // bring window to the front
         this.element.css({ "z-index": 1000 });  // TODO: should be less than taskbar
     },
 
-    unselect: function() {
-        this.task_element.removeClass( "selected" );
-        this.element.css({ "z-index": 0 });
+    unselect: function( element ) {
+        element = $( element );
+        var task_element = $( "div[for='" + element.attr( "id" ) + "'].task" );
+        if (task_element) {
+            task_element.removeClass( "selected" );
+        }
+        element.css({ "z-index": 0 });
     },
 
     taskClick: function() {
         if (this.task_element.is( ".selected" )) {
-            this.unselect();
+            this.unselect( this.element );
             this.element.hide();
         }
         else {
